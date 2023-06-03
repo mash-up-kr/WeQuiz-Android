5d14e9d77fb6b21cd7c0b1b9406db22c03a276be
jisungbin <ji@sungb.in>
2023-06-03 15:15:53 +0900

    홈 모듈 및 컨벤션 플러그인 등록

---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+rootProject.name = "build-logic"
+
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
