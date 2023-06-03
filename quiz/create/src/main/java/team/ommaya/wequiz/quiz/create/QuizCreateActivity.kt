be6657a05a8df77691eb2cd6f6d7e297fffd89a7
Kim Hyun Su <jwzz9324@tukorea.ac.kr>
2023-06-03 16:25:54 +0900

    copyright추가

--- quiz/create/src/main/java/team/ommaya/wequiz/quiz/create/QuizCreateActivity.kt
+++ quiz/create/src/main/java/team/ommaya/wequiz/quiz/create/QuizCreateActivity.kt
@@ -1,9 +1,16 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
 package team.ommaya.wequiz.quiz.create
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 
-class QuizCreateActivity : AppCompatActivity() {
+class QuizCreateActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_create)
