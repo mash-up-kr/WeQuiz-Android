af97d0a91243b172a38da3c0009189d79df081fa
Kim Hyun Su <jwzz9324@tukorea.ac.kr>
2023-06-03 16:11:58 +0900

    모듈 세팅 리뷰반영
    성빈이 리뷰 반영

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,6 @@ plugins {
     alias(libs.plugins.kotlin.detekt)
     alias(libs.plugins.kotlin.ktlint)
     alias(libs.plugins.gradle.dependency.handler.extensions)
-    id("com.android.library") version "8.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
 
 buildscript {
