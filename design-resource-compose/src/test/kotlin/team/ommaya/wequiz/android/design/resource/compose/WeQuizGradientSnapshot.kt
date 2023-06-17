394b0350b01330c8d49d43cfc944d9398e421a50
jisungbin <ji@sungb.in>
2023-06-17 10:35:36 +0900

    그라데이션 리소스 스냅샷 추가

---
+++ design-resource-compose/src/test/kotlin/team/ommaya/wequiz/android/design/resource/compose/WeQuizGradientSnapshot.kt
@@ -0,0 +1,45 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+@file:Suppress("TestFunctionName")
+
+package team.ommaya.wequiz.android.design.resource.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WeQuizGradientSnapshot {
+    @Test
+    fun Black() {
+        captureRoboImage("src/test/snapshots/WeQuizGradient/black.png") {
+            Box(
+                Modifier
+                    .size(300.dp)
+                    .background(brush = WeQuizGradient.Black),
+            )
+        }
+    }
+
+    @Test
+    fun Secondary() {
+        captureRoboImage("src/test/snapshots/WeQuizGradient/secondary.png") {
+            Box(
+                Modifier
+                    .size(300.dp)
+                    .background(brush = WeQuizGradient.Secondary),
+            )
+        }
+    }
+}
