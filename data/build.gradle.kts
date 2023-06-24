a596e97d467b1165fda760664f492c43ecee6f1c
jisungbin <ji@sungb.in>
2023-06-24 17:32:49 +0900

    data 및 domain 모듈 추가

---
+++ data/build.gradle.kts
@@ -0,0 +1,35 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+@file:Suppress("INLINE_FROM_HIGHER_PLATFORM", "UnstableApiUsage")
+
+plugins {
+    android("library")
+    kotlin("android")
+}
+
+android {
+    namespace = "team.ommaya.wequiz.android.data"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    sourceSets {
+        getByName("main").java.srcDir("src/main/kotlin")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+}
