fa5ebe6c579df6dcac040da3ae97e9c7edb33935
Kim Hyun Su <jwzz9324@tukorea.ac.kr>
2023-06-26 23:13:15 +0900

    BaseActivity 추가

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/base/BaseViewBindingActivity.kt
@@ -0,0 +1,35 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.base
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewBindingActivity<VB : ViewBinding>(
+    private val bindingInflater: (inflater: LayoutInflater) -> VB,
+) : Activity() {
+
+    private var _binding: VB? = null
+
+    protected val binding: VB
+        get() = _binding ?: error("Binding is null")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
