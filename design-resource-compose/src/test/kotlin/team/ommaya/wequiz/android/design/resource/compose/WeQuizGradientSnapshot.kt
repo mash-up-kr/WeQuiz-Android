9d0b8976d6152feed17a8fb72a7c5215720bae27
KimHance <jwzz9324@tukorea.ac.kr>
2023-06-27 00:37:37 +0900

    .png파일 삭제

--- design-resource-compose/src/test/kotlin/team/ommaya/wequiz/android/design/resource/compose/WeQuizGradientSnapshot.kt
+++ design-resource-compose/src/test/kotlin/team/ommaya/wequiz/android/design/resource/compose/WeQuizGradientSnapshot.kt
@@ -16,10 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@Ignore
 class WeQuizGradientSnapshot {
     @Test
     fun Black() {
