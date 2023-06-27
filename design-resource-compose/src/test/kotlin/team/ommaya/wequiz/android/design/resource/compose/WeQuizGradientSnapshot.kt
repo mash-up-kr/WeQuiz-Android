35cbb9d3fb570c473cb3391a1679a346e8edc557
Kim Hyun Su <86879099+KimHance@users.noreply.github.com>
2023-06-27 15:53:19 +0900

    공통 버튼 스타일 작성 (#59)
    * .png파일 삭제
    
    * @SuppressLint(IgnoreWithoutReason) 추가
    
    * [공통 #49] RoundButton 스타일 작성
    
    * [공통 #49] RoundButton 스타일 작성
    
    * [공통 #49] MaxButton 스타일 작성
    
    * [공통 #49] 빈 줄 제거

--- design-resource-compose/src/test/kotlin/team/ommaya/wequiz/android/design/resource/compose/WeQuizGradientSnapshot.kt
+++ design-resource-compose/src/test/kotlin/team/ommaya/wequiz/android/design/resource/compose/WeQuizGradientSnapshot.kt
@@ -9,6 +9,7 @@
 
 package team.ommaya.wequiz.android.design.resource.compose
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
@@ -16,10 +17,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.takahirom.roborazzi.captureRoboImage
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@SuppressLint("IgnoreWithoutReason")
 @RunWith(AndroidJUnit4::class)
+@Ignore
 class WeQuizGradientSnapshot {
     @Test
     fun Black() {
