80ef312a403e3c2a072bf11e618c91bd970809a7
KimHance <jwzz9324@tukorea.ac.kr>
2023-06-30 02:33:17 +0900

    [문제 만들기 #43] 1차 xml 작성

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/quiz/create/QuizCreateActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.quiz.create
+
+import team.ommaya.wequiz.android.base.BaseViewBindingActivity
+import team.ommaya.wequiz.android.databinding.ActivityQuizCreateBinding
+
+class QuizCreateActivity :
+    BaseViewBindingActivity<ActivityQuizCreateBinding>(ActivityQuizCreateBinding::inflate) {
+}
