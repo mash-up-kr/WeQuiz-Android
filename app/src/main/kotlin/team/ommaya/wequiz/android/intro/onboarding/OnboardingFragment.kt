2d0f2ff9025c69cde09582d57d5676c6d2b6c1dd
ze-zeh <chojh922@gmail.com>
2023-06-30 19:45:37 +0900

    [회원가입/로그인 #56] OnboardingFragment 추가

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/onboarding/OnboardingFragment.kt
@@ -0,0 +1,27 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.intro.onboarding
+
+import android.os.Bundle
+import android.view.View
+import team.ommaya.wequiz.android.base.BaseViewBindingFragment
+import team.ommaya.wequiz.android.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment :
+    BaseViewBindingFragment<FragmentOnboardingBinding>(FragmentOnboardingBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
