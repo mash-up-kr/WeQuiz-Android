073eb2e060ce2f2f8033f69c1ba068d3e4e04b12
ze-zeh <chojh922@gmail.com>
2023-06-30 19:35:58 +0900

    [회원가입/로그인 #56] IntroActivity 추가

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/IntroActivity.kt
@@ -0,0 +1,11 @@
+package team.ommaya.wequiz.android.intro
+
+import android.os.Bundle
+import team.ommaya.wequiz.android.base.BaseViewBindingActivity
+import team.ommaya.wequiz.android.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseViewBindingActivity<ActivityIntroBinding>(ActivityIntroBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
