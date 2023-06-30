6577373c257ebab874f868cfe18fc0ed207c71eb
KimHance <jwzz9324@tukorea.ac.kr>
2023-07-01 00:13:11 +0900

    [문제 만들기 #43] 문제 입력 아이템 어댑터 적용

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/quiz/create/QuizCreateAdapter.kt
@@ -0,0 +1,89 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.quiz.create
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import team.ommaya.wequiz.android.databinding.ItemQuizCreateAddBinding
+import team.ommaya.wequiz.android.databinding.ItemQuizCreateQuizBinding
+import team.ommaya.wequiz.android.quiz.create.viewholder.QuizAddViewHolder
+import team.ommaya.wequiz.android.quiz.create.viewholder.QuizCreateViewHolder
+
+class QuizCreateAdapter() : ListAdapter<Quiz, ViewHolder>(quizDiffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return if (viewType == Quiz.QuizType.Create.typeNum) {
+            QuizCreateViewHolder(
+                ItemQuizCreateQuizBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false,
+                )
+            )
+        } else {
+            QuizAddViewHolder(
+                ItemQuizCreateAddBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false,
+                )
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (holder is QuizCreateViewHolder) {
+            holder.bind(getItem(position))
+        }
+        if (holder is QuizAddViewHolder) {
+            holder.bind(getItem(position))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return getItem(position).type.typeNum
+    }
+
+    companion object {
+        val quizDiffCallback = object : DiffUtil.ItemCallback<Quiz>() {
+            override fun areItemsTheSame(oldItem: Quiz, newItem: Quiz): Boolean =
+                oldItem.index == newItem.index
+
+            override fun areContentsTheSame(oldItem: Quiz, newItem: Quiz): Boolean =
+                oldItem == newItem
+
+        }
+    }
+}
+
+
+data class Quiz(
+    val index: Int = 0,
+    val title: String = "",
+    val answerList: List<Answer> = emptyList(),
+    val type: QuizType = QuizType.Create,
+) {
+    sealed interface QuizType {
+        val typeNum: Int
+
+        object Create : QuizType {
+            override val typeNum = 0
+        }
+
+        object Add : QuizType {
+            override val typeNum = 1
+        }
+    }
+}
+
+data class Answer(
+    val index: Int = 0,
+    val answer: String = "",
+)
---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/quiz/create/viewholder/QuizCreateViewHolder.kt
@@ -0,0 +1,28 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.quiz.create.viewholder
+
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import team.ommaya.wequiz.android.databinding.ItemQuizCreateQuizBinding
+import team.ommaya.wequiz.android.quiz.create.Quiz
+
+class QuizCreateViewHolder(
+    private val binding: ItemQuizCreateQuizBinding,
+) : ViewHolder(binding.root) {
+
+    fun bind(item: Quiz) {
+
+    }
+}
