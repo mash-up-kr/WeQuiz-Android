32aeca508441a5aa420a77dfda5043211775dd50
ze-zeh <chojh922@gmail.com>
2023-06-30 20:21:31 +0900

    [회원가입/로그인 #56] JoinFragment, WelcomeFragment 추가

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/join/JoinFragment.kt
@@ -0,0 +1,19 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.intro.join
+
+import android.os.Bundle
+import android.view.View
+import team.ommaya.wequiz.android.base.BaseViewBindingFragment
+import team.ommaya.wequiz.android.databinding.FragmentJoinBinding
+
+class JoinFragment : BaseViewBindingFragment<FragmentJoinBinding>(FragmentJoinBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
