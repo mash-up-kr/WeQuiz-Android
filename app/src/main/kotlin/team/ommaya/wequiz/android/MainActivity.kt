616eac05d973201ff7d7ce5f4f5ca72306fc9785
jisungbin <ji@sungb.in>
2023-07-02 02:39:33 +0900

    액티비티 롤백

--- app/src/main/kotlin/team/ommaya/wequiz/android/MainActivity.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/MainActivity.kt
@@ -16,28 +16,5 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("SetTextI18n")
         setContentView(TextView(this).apply { text = "Hello, World!" })
-        /*setContent {
-            var deleteMode by remember { mutableStateOf(false) }
-
-            ExamList(
-                modifier = Modifier
-                    .background(color = WeQuizColor.G9.value)
-                    .padding(20.dp)
-                    .fillMaxSize()
-                    .clickable { deleteMode = !deleteMode },
-                exams = List(30) { index ->
-                    @Suppress("NAME_SHADOWING")
-                    val index = index + 1
-                    val boolean = index % 2 == 0
-                    ExamNameAndIsWritingPair(
-                        *//*examName = *//*
-                        "${index}번${"_시험지".repeat(10)}".take(if (!boolean) 10 else 38),
-                        *//*isWip = *//*
-                        boolean,
-                    )
-                }.toPersistentList(),
-                deleteModeEnable = deleteMode,
-            )
-        }*/
     }
 }
