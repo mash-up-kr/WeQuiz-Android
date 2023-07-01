886f13bf3f2137300455df898a2149d6fa596468
ze-zeh <chojh922@gmail.com>
2023-07-01 21:38:34 +0900

    [회원가입/로그인 #56] Phone fragment 확장 함수 적용

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/phone/PhoneFragment.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/phone/PhoneFragment.kt
@@ -9,11 +9,24 @@ package team.ommaya.wequiz.android.intro.phone
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import team.ommaya.wequiz.android.R
 import team.ommaya.wequiz.android.base.BaseViewBindingFragment
 import team.ommaya.wequiz.android.databinding.FragmentPhoneBinding
+import team.ommaya.wequiz.android.utils.formatTextAsPhoneNumber
+import team.ommaya.wequiz.android.utils.setRequestVerifyCodeButtonEnable
 
 class PhoneFragment : BaseViewBindingFragment<FragmentPhoneBinding>(FragmentPhoneBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.etPhoneInput.apply {
+            formatTextAsPhoneNumber()
+            setRequestVerifyCodeButtonEnable(button = binding.btnPhoneRequestVerifyCode)
+        }
+
+        binding.btnPhoneRequestVerifyCode.setOnClickListener {
+            findNavController().navigate(R.id.action_phoneFragment_to_verifyCodeFragment)
+        }
     }
 }
