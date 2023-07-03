b175c8a418922f1d5fd07bb8c8e5ac6f898b2041
ze-zeh <chojh922@gmail.com>
2023-07-03 18:10:33 +0900

    [회원가입/로그인 #56] edittext 포커스 여부에 따라 버튼 style 변경

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/phone/PhoneFragment.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/phone/PhoneFragment.kt
@@ -9,24 +9,66 @@ package team.ommaya.wequiz.android.intro.phone
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.navigation.fragment.findNavController
 import team.ommaya.wequiz.android.R
 import team.ommaya.wequiz.android.base.BaseViewBindingFragment
 import team.ommaya.wequiz.android.databinding.FragmentPhoneBinding
+import team.ommaya.wequiz.android.utils.KeyboardVisibilityUtils
 import team.ommaya.wequiz.android.utils.formatTextAsPhoneNumber
 import team.ommaya.wequiz.android.utils.setRequestVerifyCodeButtonEnable
 
 class PhoneFragment : BaseViewBindingFragment<FragmentPhoneBinding>(FragmentPhoneBinding::inflate) {
+    lateinit var keyboardVisibilityUtils: KeyboardVisibilityUtils
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.etPhoneInput.apply {
-            formatTextAsPhoneNumber()
-            setRequestVerifyCodeButtonEnable(button = binding.btnPhoneRequestVerifyCode)
-        }
+        initView()
+        initKeyboardVisibilityUtils()
+    }
 
-        binding.btnPhoneRequestVerifyCode.setOnClickListener {
-            findNavController().navigate(R.id.action_phoneFragment_to_verifyCodeFragment)
+    private fun initView() {
+        binding.apply {
+            etPhoneInput.apply {
+                formatTextAsPhoneNumber()
+                setRequestVerifyCodeButtonEnable(button = binding.btnPhoneRequestVerifyCode)
+            }
+
+            btnPhoneRequestVerifyCode.setOnClickListener {
+                findNavController().navigate(R.id.action_phoneFragment_to_verifyCodeFragment)
+            }
         }
+    }
+
+    private fun initKeyboardVisibilityUtils() {
+        // 임시 코드..
+        // button의 style 바꾸는게 쉽지 않아서 일단 구현해놨는데 custom view로 변경 해야 할듯
+        val params = binding.btnPhoneRequestVerifyCode.layoutParams as MarginLayoutParams
+
+        keyboardVisibilityUtils = KeyboardVisibilityUtils(
+            window = requireActivity().window,
+            onShowKeyboard = {
+                binding.btnPhoneRequestVerifyCode.setBackgroundResource(team.ommaya.wequiz.android.design.resource.R.color.selector_bottom_btn_text_color)
+                params.setMargins(0, 0, 0, 0)
+                binding.btnPhoneRequestVerifyCode.layoutParams = params
+            },
+            onHideKeyboard = {
+                binding.btnPhoneRequestVerifyCode.setBackgroundResource(team.ommaya.wequiz.android.design.resource.R.drawable.btn_radius_12)
+                params.setMargins(dpToPx(20), dpToPx(12), dpToPx(20), dpToPx(20))
+                binding.btnPhoneRequestVerifyCode.layoutParams = params
+                binding.etPhoneInput.clearFocus()
+            }
+        )
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        val scale = resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        keyboardVisibilityUtils.detachKeyboardListeners()
     }
 }
