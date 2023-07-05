1ba53688bd61d38e2305f0364e71e615cbdbb355
ze-zeh <chojh922@gmail.com>
2023-07-05 18:01:54 +0900

    [회원가입/로그인 #56] IntroViewModel 추가

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/IntroActivity.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/IntroActivity.kt
@@ -1,10 +1,13 @@
 package team.ommaya.wequiz.android.intro
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import team.ommaya.wequiz.android.base.BaseViewBindingActivity
 import team.ommaya.wequiz.android.databinding.ActivityIntroBinding
 
 class IntroActivity : BaseViewBindingActivity<ActivityIntroBinding>(ActivityIntroBinding::inflate) {
+    private val introViewModel: IntroViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/IntroViewModel.kt
@@ -0,0 +1,14 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.intro
+
+import androidx.lifecycle.ViewModel
+
+class IntroViewModel : ViewModel() {
+
+}
