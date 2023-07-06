4eb87a5dfc115a2c9595d42b8b3436d34fddda3b
ze-zeh <chojh922@gmail.com>
2023-07-06 18:25:13 +0900

    [회원가입/로그인 #56] 키보드 상태에 따른 버튼 style 변경(임시)

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/join/JoinFragment.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/join/JoinFragment.kt
@@ -9,17 +9,22 @@ package team.ommaya.wequiz.android.intro.join
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import team.ommaya.wequiz.android.R
 import team.ommaya.wequiz.android.base.BaseViewBindingFragment
 import team.ommaya.wequiz.android.databinding.FragmentJoinBinding
+import team.ommaya.wequiz.android.utils.KeyboardVisibilityUtils
 import team.ommaya.wequiz.android.utils.setJoinNextButtonEnable
 
 class JoinFragment : BaseViewBindingFragment<FragmentJoinBinding>(FragmentJoinBinding::inflate) {
+    private lateinit var keyboardVisibilityUtils: KeyboardVisibilityUtils
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        initKeyboardVisibilityUtils()
     }
 
     private fun initView() {
@@ -30,6 +35,8 @@ class JoinFragment : BaseViewBindingFragment<FragmentJoinBinding>(FragmentJoinBi
                 if (hasFocus) {
                     textInputLayoutJoinInputNickname.isCounterEnabled = true
                     textInputLayoutJoinInputIntroduction.isCounterEnabled = false
+                } else {
+                    textInputLayoutJoinInputNickname.isCounterEnabled = false
                 }
             }
 
@@ -37,6 +44,8 @@ class JoinFragment : BaseViewBindingFragment<FragmentJoinBinding>(FragmentJoinBi
                 if (hasFocus) {
                     textInputLayoutJoinInputNickname.isCounterEnabled = false
                     textInputLayoutJoinInputIntroduction.isCounterEnabled = true
+                } else {
+                    textInputLayoutJoinInputIntroduction.isCounterEnabled = false
                 }
             }
 
@@ -48,5 +57,37 @@ class JoinFragment : BaseViewBindingFragment<FragmentJoinBinding>(FragmentJoinBi
                 findNavController().popBackStack()
             }
         }
+    }
+
+    private fun initKeyboardVisibilityUtils() {
+        // 임시 코드..
+        // button의 style 바꾸는게 쉽지 않아서 일단 구현해놨는데 custom view로 변경 해야 할듯
+        val params = binding.btnJoinNext.layoutParams as ViewGroup.MarginLayoutParams
+
+        keyboardVisibilityUtils = KeyboardVisibilityUtils(
+            window = requireActivity().window,
+            onShowKeyboard = {
+                binding.btnJoinNext.setBackgroundResource(team.ommaya.wequiz.android.design.resource.R.color.selector_bottom_btn_text_color)
+                params.setMargins(0, 0, 0, 0)
+                binding.btnJoinNext.layoutParams = params
+            },
+            onHideKeyboard = {
+                binding.btnJoinNext.setBackgroundResource(team.ommaya.wequiz.android.design.resource.R.drawable.btn_radius_12)
+                params.setMargins(dpToPx(20), dpToPx(12), dpToPx(20), dpToPx(20))
+                binding.btnJoinNext.layoutParams = params
+                binding.etJoinInputNickname.clearFocus()
+                binding.etJoinInputIntroduction.clearFocus()
+            }
+        )
+    }
+
+    private fun dpToPx(dp: Int): Int {
+        val scale = resources.displayMetrics.density
+        return (dp * scale + 0.5f).toInt()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        keyboardVisibilityUtils.detachKeyboardListeners()
     }
 }
