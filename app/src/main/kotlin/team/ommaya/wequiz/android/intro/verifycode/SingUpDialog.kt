f2c4ee61633171a4a14644f4d94100539772e1f2
ze-zeh <chojh922@gmail.com>
2023-07-07 23:09:32 +0900

    [회원가입/로그인 #75] 충돌 수정

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/verifycode/SingUpDialog.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/verifycode/SingUpDialog.kt
@@ -5,39 +5,23 @@
  * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
  */
 
-/*
- * Designed and developed by "옴마야" Team 2023.
- *
- * Licensed under the MIT.
- * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
- */
-
 package team.ommaya.wequiz.android.intro.verifycode
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowManager
-import androidx.fragment.app.activityViewModels
 import team.ommaya.wequiz.android.base.BaseViewBindingDialogFragment
 import team.ommaya.wequiz.android.databinding.DialogSignUpBinding
-import team.ommaya.wequiz.android.intro.IntroViewModel
 
 class SignUpDialog :
     BaseViewBindingDialogFragment<DialogSignUpBinding>(DialogSignUpBinding::inflate) {
-    private val introViewModel: IntroViewModel by activityViewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = super.onCreateView(inflater, container, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         initView()
-        return binding
     }
 
     private fun initView() {
