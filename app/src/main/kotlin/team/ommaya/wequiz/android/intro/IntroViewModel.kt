77f6372c3b72331038dc5a60353f47feb00fc813
ze-zeh <chojh922@gmail.com>
2023-07-18 01:17:26 +0900

    [공통 #86] 타이머 구현

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/IntroViewModel.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/IntroViewModel.kt
@@ -8,7 +8,9 @@
 package team.ommaya.wequiz.android.intro
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class IntroViewModel : ViewModel() {
@@ -18,12 +20,26 @@ class IntroViewModel : ViewModel() {
     private val _isVerificationSucceed: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isVerificationSucceed = _isVerificationSucceed.asStateFlow()
 
+    private val _isVerifyTimeOut: MutableSharedFlow<Boolean> = MutableSharedFlow()
+    var isVerifyTimeOut = _isVerifyTimeOut.asSharedFlow()
+
+    private val _verifyTime: MutableStateFlow<String> = MutableStateFlow(VERIFY_TIME)
+    val verifyTime = _verifyTime.asStateFlow()
+
     fun setStartMode(mode: IntroMode) {
         _mode.value = mode
     }
 
     fun setVerificationSucceed(isSucceed: Boolean) {
         _isVerificationSucceed.value = isSucceed
+    }
+
+    fun setVerifyTime(time: String) {
+        _verifyTime.value = time
+    }
+
+    companion object {
+        const val VERIFY_TIME = "3:00"
     }
 }
 
--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/verifycode/VerifyCodeFragment.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/verifycode/VerifyCodeFragment.kt
@@ -11,34 +11,91 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import team.ommaya.wequiz.android.base.BaseViewBindingFragment
 import team.ommaya.wequiz.android.databinding.FragmentVerifyCodeBinding
 import team.ommaya.wequiz.android.intro.IntroViewModel
+import team.ommaya.wequiz.android.utils.SnackbarMode
+import team.ommaya.wequiz.android.utils.WeQuizSnackbar
 import team.ommaya.wequiz.android.utils.isValidInputLength
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class VerifyCodeFragment :
     BaseViewBindingFragment<FragmentVerifyCodeBinding>(FragmentVerifyCodeBinding::inflate) {
     private val introViewModel: IntroViewModel by activityViewModels()
 
+    private val timer: Job = Job()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startTime()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initView()
+        collectFlow()
+    }
+
+    private fun startTime() {
+        if (timer.isActive) timer.cancel()
+
+        lifecycleScope.launch {
+            var currentTimer = START_TIME
+
+            while (currentTimer != END_TIME) {
+                delay(TIMER_INTERVAL)
+                currentTimer -= TIMER_INTERVAL
+                introViewModel.setVerifyTime(formatMilliseconds(currentTimer))
+                introViewModel.isVerifyTimeOut
+            }
+
+            WeQuizSnackbar.make(binding.root, "인증번호를 재전송했어요", SnackbarMode.Failure).show()
+            timer.cancel()
+        }
     }
 
     private fun initView() {
-        with(binding.etVerifyCodeInput) {
-            addTextChangedListener {
-                if (isValidInputLength(text.toString(), VERIFY_CODE_LENGTH)) {
-                    if (text.toString() == TEST_VERIFY_CODE) {
-                        onVerificationSucceed()
-                    } else {
-                        // 인증번호가 올바르지 않아요 snack bar trigger
+        with(binding) {
+            with(etVerifyCodeInput) {
+                addTextChangedListener {
+                    val text = text.toString()
+
+                    if (isValidInputLength(text, VERIFY_CODE_LENGTH)) {
+                        if (text == TEST_VERIFY_CODE) {
+                            onVerificationSucceed()
+                        } else {
+                            // 인증번호가 올바르지 않아요 snack bar trigger
+                        }
                     }
                 }
             }
         }
+    }
+
+    private fun collectFlow() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                introViewModel.verifyTime.collect { time ->
+                    binding.tvVerifyCodeTimer.text = time
+                }
+            }
+        }
+    }
+
+    private fun formatMilliseconds(milliseconds: Long): String {
+        val format = SimpleDateFormat("m:ss", Locale.KOREA)
+        return format.format(Date(milliseconds))
     }
 
     private fun onVerificationSucceed() {
@@ -48,6 +105,9 @@ class VerifyCodeFragment :
 
     companion object {
         const val VERIFY_CODE_LENGTH = 6
+        const val TIMER_INTERVAL = 100L
+        const val START_TIME = 18_000L
+        const val END_TIME = 0L
         const val TEST_VERIFY_CODE = "123456"
     }
 }
