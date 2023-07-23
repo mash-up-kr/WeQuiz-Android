2b0075518e378e8d5f525aecb3cb1d6bd68ca179
Kim Hyun Su <jwzz9324@tukorea.ac.kr>
2023-07-23 18:48:36 +0900

    [문제 만들기 #72] 시험지 완성 뷰

---
+++ app/src/main/kotlin/team/ommaya/wequiz/android/quiz/create/QuizCreateFinishFragment.kt
@@ -0,0 +1,11 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.quiz.create
+
+class QuizCreateFinishFragment {
+}
