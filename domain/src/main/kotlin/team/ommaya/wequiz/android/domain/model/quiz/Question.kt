1a8b4b7d94df1a5794bcde03bcaa79af5edc5919
Kim Hyun Su <jwzz9324@tukorea.ac.kr>
2023-07-23 23:37:17 +0900

    [문제 만들기 #97] 사용될 모델 추가

---
+++ domain/src/main/kotlin/team/ommaya/wequiz/android/domain/model/quiz/Question.kt
@@ -0,0 +1,15 @@
+/*
+ * Designed and developed by "옴마야" Team 2023.
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/mash-up-kr/WeQuiz-Android/blob/main/LICENSE
+ */
+
+package team.ommaya.wequiz.android.domain.model.quiz
+
+data class Question(
+    val title: String = "",
+    val priority: Int = 0,
+    val duplicatedOption: Int = 0,
+    val options: List<Option> = emptyList(),
+)
