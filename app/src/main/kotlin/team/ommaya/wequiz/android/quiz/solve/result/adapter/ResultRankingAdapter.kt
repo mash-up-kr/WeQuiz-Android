9fa4e0d993185391752222e6a5fd1d7f274dcdb0
ze-zeh <chojh922@gmail.com>
2023-07-29 16:55:49 +0900

    [시험지 풀기 #88] ci 에러 수정

--- app/src/main/kotlin/team/ommaya/wequiz/android/quiz/solve/result/adapter/ResultRankingAdapter.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/quiz/solve/result/adapter/ResultRankingAdapter.kt
@@ -24,7 +24,7 @@ class ResultRankingAdapter :
                 LayoutInflater.from(parent.context),
                 parent,
                 false,
-            )
+            ),
         )
     }
 
