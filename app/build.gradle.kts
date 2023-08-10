a12d5c66e04ac95b8d1d9538972735fa613d2ccb
ze-zeh <chojh922@gmail.com>
2023-08-10 12:54:28 +0900

    1.1.0 버전 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildFeatures {
