24188c9f7b89507db37a1a263a3db56c75e35026
ze-zeh <chojh922@gmail.com>
2023-08-11 21:38:10 +0900

    [회원가입/로그인] 디자인 QA #145

--- app/src/main/kotlin/team/ommaya/wequiz/android/intro/onboarding/OnboardingFragment.kt
+++ app/src/main/kotlin/team/ommaya/wequiz/android/intro/onboarding/OnboardingFragment.kt
@@ -14,8 +14,11 @@
 
 package team.ommaya.wequiz.android.intro.onboarding
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,10 +32,44 @@ import team.ommaya.wequiz.android.intro.IntroViewModel
 class OnboardingFragment :
     BaseViewBindingFragment<FragmentOnboardingBinding>(FragmentOnboardingBinding::inflate) {
     private val introViewModel: IntroViewModel by activityViewModels()
+    private var statusBarColor = Color.TRANSPARENT
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setStatusBarColor(true)
+        setNavigationBarPadding()
+        initView()
+    }
+
+    private fun setNavigationBarPadding() {
+        val params = binding.fragmentOnboarding.layoutParams as ViewGroup.MarginLayoutParams
+        params.setMargins(0, 0, 0, getNavigationBarHeight())
+        binding.fragmentOnboarding.layoutParams = params
+    }
+
+    private fun getNavigationBarHeight(): Int {
+        val resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        return if (resourceId > 0) {
+            resources.getDimensionPixelSize(resourceId)
+        } else 0
+    }
+
+    private fun setStatusBarColor(isTransparent: Boolean) {
+        val window = requireActivity().window
+        statusBarColor = window.statusBarColor
+
+        if (isTransparent) {
+            statusBarColor = window.statusBarColor
+            window.statusBarColor = Color.TRANSPARENT
+        } else {
+            window.statusBarColor = statusBarColor
+        }
+
+        WindowCompat.setDecorFitsSystemWindows(window, !isTransparent)
+    }
+
+    private fun initView() {
         with(binding) {
             btnOnboardingSignUp.setOnClickListener {
                 introViewModel.setStartMode(IntroMode.SIGNUP)
@@ -44,5 +81,11 @@ class OnboardingFragment :
                 findNavController().navigate(R.id.action_onboardingFragment_to_phoneFragment)
             }
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        setStatusBarColor(false)
     }
 }
